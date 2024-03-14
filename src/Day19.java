fa417045e48a5c1d687eb7460db60452f0239e2c
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-14 19:32:06 +0100

    Day 19.

--- src/Day19.java
+++ src/Day19.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Day19 {
@@ -14,11 +16,31 @@ public class Day19 {
         List<Rule> rules = Arrays.stream(sp).map(Rule::new).collect(Collectors.toList());
         String molecule = input.get(1);
         first(molecule, rules);
+        second(molecule);
     }
 
     private static void first(String molecule, List<Rule> rules) {
         Set<String> molecules = singleReplacement(molecule, rules);
         System.out.println(molecules.size());
+    }
+
+    private static void second(String molecule) {
+        Pattern pattern = Pattern.compile("[A-Z][a-z]*");
+        Matcher matcher = pattern.matcher(molecule);
+        int count = 0;
+        int countArRn = 0;
+        int countY = 0;
+        while (matcher.find()) {
+            String group = matcher.group();
+            count++;
+            if ("Ar".equals(group) || "Rn".equals(group)) {
+                countArRn++;
+            } else if ("Y".equals(group)) {
+                countY++;
+            }
+        }
+        int result = count - countArRn - 2 * countY - 1;
+        System.out.println(result);
     }
 
     private static Set<String> singleReplacement(String molecule, List<Rule> rules) {
