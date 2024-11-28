004e86352257d7002b741fcaf934bf79599225ff
Johan de Jong <johan.de.jong@sogeti.com>
2024-11-28 19:14:45 +0100

    Day 6 using the RegionGrid.

--- src/Day6.java
+++ src/Day6.java
@@ -1,10 +1,8 @@
 import lib.InputUtil;
+import lib.RegionGrid;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Day6 {
@@ -13,88 +11,39 @@ public class Day6 {
                 .stream()
                 .map(Instruction::new)
                 .collect(Collectors.toList());
-        Grid grid = new Grid(instructions);
-        System.out.println(grid.count());
-        System.out.println(grid.totalBrightness());
+        RegionGrid<Content> grid = new RegionGrid<>(instructions.stream().map(Instruction::rectangle).collect(Collectors.toList()), Content::new);
+        for (Instruction instruction : instructions) {
+            grid.apply(instruction.rectangle(), (r, c) -> c.apply(instruction));
+        }
+        long first = grid.aggregate(0L, (r, c, v) -> v + (c.on ? r.area() : 0L));
+        long second = grid.aggregate(0L, (r, c, v) -> v + r.area() * c.brightness);
+
+        System.out.println(first);
+        System.out.println(second);
     }
 
-    static class Grid {
-        List<Integer> h;
-        List<Integer> v;
-        boolean[][] grid;
-        int[][] brightness;
+    static class Content {
+        boolean on;
+        int brightness;
 
-        public Grid(List<Instruction> instructions) {
-            SortedSet<Integer> x = new TreeSet<>();
-            SortedSet<Integer> y = new TreeSet<>();
-            for (Instruction instruction : instructions) {
-                x.add(instruction.x1);
-                y.add(instruction.y1);
-                x.add(instruction.x2 + 1);
-                y.add(instruction.y2 + 1);
-            }
-            h = new ArrayList<>(x);
-            v = new ArrayList<>(y);
-            grid = new boolean[v.size() - 1][h.size() - 1];
-            brightness = new int[v.size() - 1][h.size() - 1];
-            instructions.forEach(this::toggle);
-        }
-
-        private void toggle(Instruction instruction) {
-            int x1 = h.indexOf(instruction.x1);
-            int y1 = v.indexOf(instruction.y1);
-            int x2 = h.indexOf(instruction.x2 + 1);
-            int y2 = v.indexOf(instruction.y2 + 1);
-            for (int y = y1; y < y2; y++) {
-                for (int x = x1; x < x2; x++) {
-                    switch (instruction.operation) {
-                        case ON:
-                            grid[y][x] = true;
-                            brightness[y][x]++;
-                            break;
-                        case OFF:
-                            grid[y][x] = false;
-                            brightness[y][x]--;
-                            if (brightness[y][x] < 0) {
-                                brightness[y][x] = 0;
-                            }
-                            break;
-                        case TOGGLE:
-                            grid[y][x] = !grid[y][x];
-                            brightness[y][x] += 2;
-                            break;
+        void apply(Instruction instruction) {
+            switch (instruction.operation) {
+                case ON:
+                    on = true;
+                    brightness++;
+                    break;
+                case OFF:
+                    on = false;
+                    brightness--;
+                    if (brightness < 0) {
+                        brightness = 0;
                     }
-                }
+                    break;
+                case TOGGLE:
+                    on = !on;
+                    brightness += 2;
+                    break;
             }
-        }
-
-        public int count() {
-            int result = 0;
-            for (int y = 0; y < v.size() - 1; y++) {
-                int yy = v.get(y + 1) - v.get(y);
-                for (int x = 0; x < h.size() - 1; x++) {
-                    if (grid[y][x]) {
-                        int xx = h.get(x + 1) - h.get(x);
-                        int area = xx * yy;
-                        result += area;
-                    }
-                }
-            }
-            return result;
-        }
-
-        public long totalBrightness() {
-            long result = 0L;
-            for (int y = 0; y < v.size() - 1; y++) {
-                int yy = v.get(y + 1) - v.get(y);
-                for (int x = 0; x < h.size() - 1; x++) {
-                    int xx = h.get(x + 1) - h.get(x);
-                    long area = ((long) xx) * ((long) yy);
-                    long value = brightness[y][x];
-                    result += (area * value);
-                }
-            }
-            return result;
         }
     }
 
@@ -120,6 +69,10 @@ public class Day6 {
             } else {
                 throw new IllegalArgumentException("Unknown operation");
             }
+        }
+
+        RegionGrid.Rectangle rectangle() {
+            return new RegionGrid.Rectangle(x1, y1, x2 + 1, y2 + 1);
         }
     }
 
